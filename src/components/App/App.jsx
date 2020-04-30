0d21855757c6117ac53e25f0e4019473343c94f7
Josh Ford <thisisjoshford@gmail.com>
2020-04-30 15:16:53 -0700

    header render

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,12 @@
 import React from 'react';
 import Form from '../Form/Form';
+import Header from '../Header/Header';
 
 export default function App() {
   return (
-    <Form/>
+    <>
+      <Header/>
+      <Form/>
+    </>
   );
 }
