44b93523db257842e9ffc04f6e01aae1e0bf32b1
Josh Ford <thisisjoshford@gmail.com>
2020-05-10 23:30:42 -0700

    history item

---
+++ src/components/History/HistoryItem.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+const HistoryItem = ({ url, method }) => (
+  <section>
+    <p>{url}</p>
+    <p>{method}</p>
+  </section>
+);
+
+HistoryItem.propTypes = {
+  url: PropTypes.string.isRequired
+};
+
+export default HistoryItem;
